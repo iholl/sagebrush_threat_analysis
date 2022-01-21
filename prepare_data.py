6cac8e8bb229d41f017a857d5e86ed36b9de7b16
Ian Michael Holl <ian.michael.holl@gmail.com>
2022-01-21 12:22:30 -0800

    Added: script to prep data for avg and .gitignore

---
+++ prepare_data.py
@@ -0,0 +1,36 @@
+import os
+import rasterio
+
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+def extract_bands(multiband_raster):
+    dataset = rasterio.open("data/{}".format(multiband_raster)) 
+    
+    for band in band_list:  
+        source_band = dataset.read(band)
+
+        folder_name = "band_{}".format(band)
+        create_folder(folder_name)
+        
+        with rasterio.open(
+            "{}/{}".format(folder_name, multiband_raster),
+            "w",
+            driver="GTiff",
+            height=source_band.shape[0],
+            width=source_band.shape[1],
+            count=1,
+            dtype=source_band.dtype,
+            crs=dataset.crs,
+            transform=dataset.transform,
+        ) as dst:
+            dst.write(source_band, 1)
+    
+# list of bands to be extracted: annuals, perennials, shrubs and trees
+band_list = [1,4,5,6]
+#create a list of all the rasters in the data folder
+raster_list = os.listdir("data")
+# function to extract the bands from a raster is used twice in the code
+for raster in raster_list:
+    extract_bands(raster)
