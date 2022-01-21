80a712a07115b8e0a9fa1f8663524c9f46b2ffd3
Ian Michael Holl <ian.michael.holl@gmail.com>
2022-01-21 11:05:46 -0800

    Init commit

---
+++ download_data.py
@@ -0,0 +1,21 @@
+import os
+from osgeo import gdal
+
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+# coordinates for bounding box to limit the extent of the data
+data_extent = [-120, 42, -119, 41]
+# date range for downloading annual data
+date_min = 2016
+date_max = 2020
+date_list = list(range(date_min, date_max + 1))
+# create a folder for the data if no does not exist
+create_folder("data")
+# loop through all the dates in the range and downloads to the data folder
+for date in date_list:
+    url = "/vsicurl/http://rangeland.ntsg.umt.edu/data/rap/rap-vegetation-cover/v3/vegetation-cover-v3-{}.tif".format(date)
+    raster = gdal.Open(url)
+    gdal.Translate("data/output_{}.tif".format(date), raster, projWin = data_extent)
+    raster = None
